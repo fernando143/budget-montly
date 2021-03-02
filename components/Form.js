acfb58e2482a3d2402421121204d17007fab938d
Fernando Casabuena <fcasabuena@goiar.com>
2021-03-01 22:47:44 -0300

    corrección deslizamiento altura en formulario, añadido funcionalidad ocultar teclado

--- components/Form.js
+++ components/Form.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, ScrollView } from 'react-native'
 import { Input, Text, Button, Icon } from 'react-native-elements'
 
 // CONSTANTS & HELPERS
@@ -33,96 +33,98 @@ const Form = ({title, isSaving, data, onCancel, onSubmit}) => {
   }
 
   return (
-    <View style={styles.container}>
-      <Text
-        h4
-        h4Style={h4Styles}
-      >
-        {title}
-      </Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text
+          h4
+          h4Style={h4Styles}
+        >
+          {title}
+        </Text>
 
-      <Input
-        label="Nombre"
-        placeholder="Ingrese un nombre"
-        leftIcon={{ type: 'font-awesome-5', name: 'file-invoice' }}
-        defaultValue={valuesForm.name ? valuesForm.name : null}
-        onChangeText={value => onChange('name', value)}
-      />
-      <GenericInput
-        label="Seleccione Mes"
-        leftIcon={{ type: 'font-awesome-5', name: 'calendar-alt' }}
-        children1={
-          <MonthPicker
-            defaultValue={valuesForm.month ? valuesForm.month : '01'}
-            onChangeText={value => onChange('month', value)}
+        <Input
+          label="Nombre"
+          placeholder="Ingrese un nombre"
+          leftIcon={{ type: 'font-awesome-5', name: 'file-invoice' }}
+          defaultValue={valuesForm.name ? valuesForm.name : null}
+          onChangeText={value => onChange('name', value)}
+        />
+        <GenericInput
+          label="Seleccione Mes"
+          leftIcon={{ type: 'font-awesome-5', name: 'calendar-alt' }}
+          children1={
+            <MonthPicker
+              defaultValue={valuesForm.month ? valuesForm.month : '01'}
+              onChangeText={value => onChange('month', value)}
+            />
+          }
+        />
+        <Input
+          label="Año"
+          placeholder="Ingrese el año"
+          leftIcon={{ type: 'font-awesome-5', name: 'calendar-alt' }}
+          keyboardType="number-pad"
+          defaultValue={valuesForm.year ? valuesForm.year : null}
+          onChangeText={value => onChange('year', value)}
+        />
+        <DateInput
+          label="Fecha de pago"
+          placeholder="Ingrese la fecha de pago"
+          leftIcon={{ type: 'font-awesome-5', name: 'calendar-alt' }}
+          defaultValue={valuesForm.datePaid ? valuesForm.datePaid : null}
+          onChange={value => onChange('datePaid', value)}
+        />
+        <Input
+          label="Importe"
+          placeholder="Ingrese el importe"
+          leftIcon={{ type: 'font-awesome', name: 'dollar' }}
+          keyboardType="number-pad"
+          defaultValue={valuesForm.mount ? valuesForm.mount : null}
+          onChangeText={value => onChange('mount', value)}
+        />
+        <Input
+          label="Observación"
+          placeholder="Ingrese alguna observación (opcional)"
+          leftIcon={{ type: 'font-awesome-5', name: 'comment-alt' }}
+          multiline={true}
+          defaultValue={valuesForm.observation ? valuesForm.observation : null}
+          onChangeText={value => onChange('observation', value)}
+        />
+
+        <View style={styles.btnContainerGuardar}>
+          <Button
+            title="Cancelar"
+            containerStyle={styles.btnContainerStyle}
+            buttonStyle={{backgroundColor: COLOR_AMERICAN_RIVER}}
+            icon={
+              <Icon
+                name="times"
+                type="font-awesome-5"
+                color="#FFF"
+                size={20}
+                containerStyle={{marginRight: 5}}
+              />
+            }
+            onPress={onCancel}
           />
-        }
-      />
-      <Input
-        label="Año"
-        placeholder="Ingrese el año"
-        leftIcon={{ type: 'font-awesome-5', name: 'calendar-alt' }}
-        keyboardType="number-pad"
-        defaultValue={valuesForm.year ? valuesForm.year : null}
-        onChangeText={value => onChange('year', value)}
-      />
-      <DateInput
-        label="Fecha de pago"
-        placeholder="Ingrese la fecha de pago"
-        leftIcon={{ type: 'font-awesome-5', name: 'calendar-alt' }}
-        defaultValue={valuesForm.datePaid ? valuesForm.datePaid : null}
-        onChange={value => onChange('datePaid', value)}
-      />
-      <Input
-        label="Importe"
-        placeholder="Ingrese el importe"
-        leftIcon={{ type: 'font-awesome', name: 'dollar' }}
-        keyboardType="number-pad"
-        defaultValue={valuesForm.mount ? valuesForm.mount : null}
-        onChangeText={value => onChange('mount', value)}
-      />
-      <Input
-        label="Observación"
-        placeholder="Ingrese alguna observación (opcional)"
-        leftIcon={{ type: 'font-awesome-5', name: 'comment-alt' }}
-        multiline={true}
-        defaultValue={valuesForm.observation ? valuesForm.observation : null}
-        onChangeText={value => onChange('observation', value)}
-      />
-
-      <View style={styles.btnContainerGuardar}>
-        <Button
-          title="Cancelar"
-          containerStyle={styles.btnContainerStyle}
-          buttonStyle={{backgroundColor: COLOR_AMERICAN_RIVER}}
-          icon={
-            <Icon
-              name="times"
-              type="font-awesome-5"
-              color="#FFF"
-              size={20}
-              containerStyle={{marginRight: 5}}
-            />
-          }
-          onPress={onCancel}
-        />
-        <Button
-          title="Guardar"
-          loading={isSaving}
-          containerStyle={styles.btnContainerStyle}
-          icon={
-            <Icon
-              name="save"
-              type="font-awesome-5"
-              color="#FFF"
-              size={20}
-              containerStyle={{marginRight: 5}}
-            />
-          }
-          onPress={handleSubmit}
-        />
+          <Button
+            title="Guardar"
+            loading={isSaving}
+            containerStyle={styles.btnContainerStyle}
+            icon={
+              <Icon
+                name="save"
+                type="font-awesome-5"
+                color="#FFF"
+                size={20}
+                containerStyle={{marginRight: 5}}
+              />
+            }
+            onPress={handleSubmit}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   )
 }
 
