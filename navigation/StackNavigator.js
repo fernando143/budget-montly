973cbba990fa021bb45f6977884ad928ae7ea2c5
Fernando Casabuena <azarias08@gmail.com>
2021-07-04 13:14:27 -0300

    añadido splash screen para android

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -11,8 +11,10 @@ import { COLOR_ELECTRON_BLUE } from '../constants'
 
 // CONTEXT
 import { AuthUserContext } from '../context/authUser'
-const version = '0.11.6'
+const version = '0.12.6'
 /*NOTAS DE LA VERSION
+0.12.6
+Feature, se añade splash screen
 0.11.6
 Fix en selector que hacia que no traiga los datos cuando solo se seleccionaba el mes o el año de
 Fix en el context dataUser que intentaba ejecutarse cuando el usuario es null, es decir cuando la app no tiene ningun correo ingresado
